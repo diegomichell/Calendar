db0e1e6972794cb05a28969f4de19aa5f0764ada
Diego Michel <diego.ivan.perez.michel@gmail.com>
2020-04-14 01:29:46 -0400

    fallback in case weather service fails

--- src/sagas/service_create_event.ts
+++ src/sagas/service_create_event.ts
@@ -10,11 +10,18 @@ import Config from '../config';
 export function* serviceCreateEvent(action: any) {
   const event: CalendarEvent = action.payload.event;
   const days = event.date.day() - moment().day();
-  const weather = yield call(WeatherService.forecast, Config.WEATHER_API_KEY, event.city, days);
-  const createdEvent = yield call(EventsService.createEvent, {
-    ...event,
-    weather
-  });
+  let createdEvent;
+  try {
+    const weather = yield call(WeatherService.forecast, Config.WEATHER_API_KEY, event.city, days);
+    createdEvent = yield call(EventsService.createEvent, {
+      ...event,
+      weather
+    });
+  } catch (e) {
+    console.log("Weather service failed");
+    console.log(e);
+    createdEvent = yield call(EventsService.createEvent, event);
+  }
 
   yield put(EventActions.createEvent(createdEvent));
 }
